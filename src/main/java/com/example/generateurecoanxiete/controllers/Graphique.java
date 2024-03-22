7b62a56c9970c1d77d2e7aef49a5df07332708fa
2130663 <2130663@etu.cegepjonquiere.ca>
2024-03-22 13:27:57 -0400

    comment mettre donnees dans diagramme a partir de liste

--- src/main/java/com/example/generateurecoanxiete/controllers/Graphique.java
+++ src/main/java/com/example/generateurecoanxiete/controllers/Graphique.java
@@ -1,22 +1,63 @@
 package com.example.generateurecoanxiete.controllers;
 
+import com.example.generateurecoanxiete.Dechet;
 import com.example.generateurecoanxiete.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Graphique {
 
     @FXML
     BorderPane borderPane;
+    @FXML
+    BarChart barChart;
+    @FXML
+    CategoryAxis axeDate;
+    @FXML
+    NumberAxis axeNombre;
+    XYChart.Series serie = new XYChart.Series();
+    List<Dechet> maPoubelle = new ArrayList<>();
+    List<String> poubelle;
+    {
+        try {
+            poubelle = Files.readAllLines(Paths.get("PoubelleUtilisateur.csv"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
     public void initialize(){
         borderPane.setBackground(new Background(new BackgroundImage(new Image("file:dechetsBG.jpg"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true))));
+
+        for (String dechet : poubelle) {
+            String[] infos = dechet.split(", ");
+            if (!dechet.isEmpty()) {
+                maPoubelle.add(new Dechet(infos[0], infos[1], new Image(infos[2]), LocalDate.now()));
+            }
+            else
+                System.out.println("La poubelle est vide.");
+        }
+
+        maPoubelle.sort(Comparator.comparing(Dechet::getDate));
+
     }
+
+
 
     public void menu() throws IOException {
         HelloApplication.changerScene("/menu.fxml");
